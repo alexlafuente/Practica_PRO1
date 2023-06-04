871f2f85a2a3efa89ab2f83c6c97bc1f9cfcb88d
Alex Lafuente <alex.lafuente.g@gmail.com>
2023-06-04 15:48:59 +0200

    Borrado comentarios en la implementacion de los metodos

--- Pacient.cpp
+++ Pacient.cpp
@@ -4,8 +4,6 @@
 // Constructors
 //-------------
 
-/* Pre: cert */
-/* Post: crea un pacient buit */		
 Pacient::Pacient(){
     nom = "";
     edat = 0;
@@ -13,8 +11,6 @@ Pacient::Pacient(){
     gravetat = 0;
 }
 
-/* Pre: cert */
-/* Post: crea un pacient de nom 'nom' */	
 Pacient::Pacient(string nom){
     this->nom = nom;
     edat = 0;
@@ -22,9 +18,7 @@ Pacient::Pacient(string nom){
     gravetat = 0;
 }
 
-/* Pre: cert */
-/* Post: crea un pacient amb els valors de 'nom',
-    'edat', 'motiu' i 'gravetat' rebuts per paràmetre */
+
 Pacient::Pacient(string nom, int edat, string motiu, int gravetat){
     this->nom = nom;
     this->edat = edat;
@@ -39,8 +33,6 @@ Pacient::Pacient(const Pacient &p){
     this->gravetat = p.getGravetat();
 }
 
-// Destructor
-// Post: esborra automaticament els objectes locals en sortir d'un ambit de visibilitat
 Pacient::~Pacient(){
     
 }
@@ -49,9 +41,6 @@ Pacient::~Pacient(){
 // Modificadors
 //-------------
 
-/* Pre: cert */
-/* Post: el valor de gravetat del pacient paràmetre 
-    implícit	passa a ser el valor de 'gravetat' */
 void Pacient::actualitzaEstat(int gravetat){
     this->gravetat = gravetat;
 }
@@ -68,36 +57,22 @@ Pacient& Pacient::operator=(const Pacient &p){
 // Consultors
 //-----------
 
-/* Pre: cert */
-/* Post: retorna el nom del pacient paràmetre implicit */
 string Pacient::getNom() const{
     return nom;
 }
 
-/* Pre: cert */
-/* Post: retorna l'edat del pacient paràmetre implicit */
 int Pacient::getEdat() const{
     return edat;
 }
 
-/* Pre: cert */
-/* Post: retorna el motiu de l'ingrés del pacient paràmetre 
-    implicit */
 string Pacient::getMotiu() const{
     return motiu;
 }
 
-/* Pre: cert */
-/* Post: retorna la gravetat de l'estat del pacient paràmetre
-    implicit */		
 int Pacient::getGravetat() const{
     return gravetat;
 }
 
-/* Pre: cert */
-/* Post: retorna un booleà que indica si l'estat de gravetat
-    del pacient paràmetre implícit i el del pacient 'p' són
-    identics (true) o no ho són (false) */
 bool Pacient::compare(const Pacient &p) const{
     return this->gravetat < p.getGravetat();
 }
@@ -110,10 +85,6 @@ bool Pacient::operator==(Pacient p) const{
     return this->nom == p.getNom();
 }
 
-/* Pre: cert */
-/* Post: retorna un booleà que indica si el pacient paràmetre
-    implícit és anterior al pacient 'p' rebut (true) o
-    no ho és (false), en base a l'ordenació alfabètica dels noms d'aquests */
 bool Pacient::operator<(Pacient p) const{
     return this->nom < p.getNom();
 }
@@ -126,19 +97,11 @@ bool Pacient::operator>(Pacient p) const{
 // L/E
 //-----------
 
-/* Pre: estan preparats al canal estàndard d'entrada quatre valors
-    en aquest ordre - string, enter, string i enter */
-/* Post: el pacient 'p' passa a tenir els valors llegits del canal
-    estàndard d'entrada com a nom, edat, motiu i gravetat, 
-    respectivament */			
 istream& operator>>(istream &is, Pacient &p){
     is >> p.nom >> p.edat >> p.motiu >> p.gravetat;
     return is;
 }
 
-/* Pre: cert */
-/* Post: s'han escrit els atributs nom, edat, motiu i gravetat del
-    pacient 'p' al canal estàndard de sortida */
 ostream& operator<<(ostream &os, const Pacient &p){
     os << p.nom << "  " << p.edat << " " << p.motiu << " " << p.gravetat;
     return os;
